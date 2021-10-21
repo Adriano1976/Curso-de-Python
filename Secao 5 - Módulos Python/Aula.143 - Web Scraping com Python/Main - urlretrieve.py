abe1b3f9d67efb618d4fdea10a98b359a9e0ced0
Adriano Santos <adriradiologista@hotmail.com.br>
2021-10-21 15:52:12 -0300

    Implementando de mensagem de saída do resultado do processamento do download requerido.

--- Secao 5 - Módulos Python/Aula.143 - Web Scraping com Python/Main - urlretrieve.py
+++ Secao 5 - Módulos Python/Aula.143 - Web Scraping com Python/Main - urlretrieve.py
@@ -4,3 +4,6 @@ print('Baixando capa do seu curso...')
 
 url = 'https://i.udemycdn.com/course/240x135/2411816_3802_3.jpg'
 urllib.request.urlretrieve(url, './seu_curso.jpg')
+
+print('Capa do seu curso baixado com sucesso!')
+
