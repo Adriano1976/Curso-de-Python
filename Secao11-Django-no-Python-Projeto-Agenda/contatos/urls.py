087f6808b08294cae4ef19b475c332d37f9b350f
Adriano Santos <adriradiologista@hotmail.com.br>
2022-02-21 17:38:59 -0300

    Projeto de criação de uma agenda usando django, html, css, bootstrap e javascript.

---
+++ Secao11-Django-no-Python-Projeto-Agenda/contatos/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# Como exibir nome ao invez da Id na barra de endereços?
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:contato_id>', views.ver_contato, name='ver_contato'),  # modifique aqui
+]
---
+++ Secao11-Django-no-Python-Projeto-Agenda/contatos/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Contato
+from django.core.paginator import Paginator
+
+
+# Create your views here.
+# Como exibir nome ao invez da Id na barra de endereços?
+
+
+def index(request):
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 20)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+
+
+def ver_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)  # modifique aqui
+
+    if not contato.mostrar:
+        raise Http404()
+
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+    })
