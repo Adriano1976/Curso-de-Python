7f249613387ce4e807a31a5d573c0b19430054b0
Adriano Santos <adriradiologista@hotmail.com.br>
2022-02-23 16:54:28 -0300

    Criando mensagem de Erro com Django Messages.

--- Secao11-Django-com-Python-Projetos/Projeto-Agenda/agenda/settings.py
+++ Secao11-Django-com-Python-Projetos/Projeto-Agenda/agenda/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,13 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Mensagens do django
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.DEBUG: 'alert-info',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
