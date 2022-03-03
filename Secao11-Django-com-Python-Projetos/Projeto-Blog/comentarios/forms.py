603d1e53cca5a6487f4f1f4caff9d40885442b42
Adriano Santos <adriradiologista@hotmail.com.br>
2022-03-03 17:56:28 -0300

    Corrigindo form de comentários

--- Secao11-Django-com-Python-Projetos/Projeto-Blog/comentarios/forms.py
+++ Secao11-Django-com-Python-Projetos/Projeto-Blog/comentarios/forms.py
@@ -1,18 +1,56 @@
+from django.forms.widgets import TextInput, EmailInput, Textarea
 from django.forms import ModelForm
 from .models import Comentario
 
 
 class FormComentario(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ''  # Para tirar : depois do nome
+
     def clean(self):
         data = self.cleaned_data
         nome = data.get('nome_comentario')
+        email = data.get('email_comentario')
+        comentario = data.get('comentario')
 
         if len(nome) < 5:
             self.add_error(
                 'nome_comentario',
-                'Nome precisa ter mais de 5 caracteres.'
+                'Nome precisa ter mais que 5 caracteres.'
             )
 
     class Meta:
         model = Comentario
         fields = ('nome_comentario', 'email_comentario', 'comentario')
+        widgets = {
+            'nome_comentario': TextInput(attrs={
+                'placeholder': 'Digite seu nome',
+                'class': 'form-control',
+            }),
+            'email_comentario': EmailInput(attrs={
+                'placeholder': 'Digite seu e-mail',
+                'class': 'form-control',
+            }),
+            'comentario': TextInput(attrs={
+                'placeholder': 'Digite seu comenário',
+                'class': 'form-control',
+            }),
+        }
+
+# class FormComentario(ModelForm):
+#     def clean(self):
+#         data = self.cleaned_data
+#         nome = data.get('nome_comentario')
+#         email = data.get('email_comentario')
+#         comentario = data.get('comentario')
+#
+#         if len(nome) < 5:
+#             self.add_error(
+#                 'nome_comentario',
+#                 'Nome precisa ter mais de 5 caracteres.'
+#             )
+#
+#     class Meta:
+#         model = Comentario
+#         fields = ('nome_comentario', 'email_comentario', 'comentario')
