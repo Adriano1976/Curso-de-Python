b669f1d3f0da2e85e7a6d9958f3aa00f7a0fa4bd
Adriano Santos <adriradiologista@hotmail.com.br>
2022-03-04 19:14:54 -0300

    Adicionado recaptcha do google.

--- Secao11-Django-com-Python-Projetos/Projeto-Blog/comentarios/forms.py
+++ Secao11-Django-com-Python-Projetos/Projeto-Blog/comentarios/forms.py
@@ -2,6 +2,7 @@ from django.core.validators import validate_email
 from django.forms.widgets import TextInput, EmailInput, Textarea
 from django.forms import ModelForm
 from .models import Comentario
+import requests
 
 
 class FormComentario(ModelForm):
@@ -10,20 +11,40 @@ class FormComentario(ModelForm):
         self.label_suffix = ''  # Para tirar : depois do nome
 
     def clean(self):
-        data = self.cleaned_data
-        nome = data.get('nome_comentario')
-        comentario = data.get('comentario')
+        raw_data = self.data
+        recaptcha_response = raw_data.get('g-recaptcha-response')
+
+        if not recaptcha_response:
+            self.add_error(
+                'comentario',
+                'Por favor, marque a caixa "Não sou um robô".'
+            )
+
+        recaptcha_request = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6LdTwbQeAAAAAFNQ_S-bCUAwSrTECkxpaEx7WnPi',
+                'response': recaptcha_response
+            }
+        )
+        recaptcha_result = recaptcha_request.json()
+
+        print(recaptcha_result)
+        print(recaptcha_result.get('success'))
+
+        if not recaptcha_result.get('success'):
+            self.add_error(
+                'comentario',
+                'Desculpe Mr. Robot, ocorreu um erro.'
+            )
+
+        cleaned_data = self.cleaned_data
+        nome = cleaned_data.get('nome_comentario')
 
         if len(nome) < 5:
             self.add_error(
                 'nome_comentario',
                 'Nome precisa ter mais que 5 caracteres.'
-            )
-
-        if len(comentario) < 5:
-            self.add_error(
-                'comentario',
-                'Comentário precisa ter mais de 5 caracteres.'
             )
 
     class Meta:
