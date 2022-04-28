3da2c3ee1dde7ccdcd06799d01ab033b2957828a
Adriano Santos <adriradiologista@hotmail.com.br>
2022-04-28 17:45:49 -0300

    Salvando pedido do carrinho do cliente.

--- Secao11-Django-com-Python-Projetos/Projeto-E-commerce/pedido/models.py
+++ Secao11-Django-com-Python-Projetos/Projeto-E-commerce/pedido/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Pedido(models.Model):
     Usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.FloatField()
+    qtd_total = models.PositiveIntegerField()
     status = models.CharField(
         default='C',
         max_length=1,
