6ff87fa045bcc985f46f98a0a0eb4929d1a68221
Adriano Santos <adriradiologista@hotmail.com.br>
2022-05-03 18:51:48 -0300

    Migrado resumo da compra do pagar para salvarpedido.

--- Secao11-Django-com-Python-Projetos/Projeto-E-commerce/pedido/views.py
+++ Secao11-Django-com-Python-Projetos/Projeto-E-commerce/pedido/views.py
@@ -9,6 +9,11 @@ from utils import utils
 
 
 class Pagar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('PAGAR')
+
+
+class SalvarPedido(View):
     template_name = 'pedido/pagar.html'
 
     def get(self, *args, **kwargs):
@@ -80,11 +85,6 @@ class Pagar(View):
 
         del self.request.session['carrinho']
         return redirect('pedido:lista')
-
-
-class SalvarPedido(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('SalvarPedido')
 
 
 class Detalhe(View):
