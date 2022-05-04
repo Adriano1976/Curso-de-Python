4d74a6ff4f36750f578d7c0b3d8970fcfddeea3e
Adriano Santos <adriradiologista@hotmail.com.br>
2022-05-04 12:56:02 -0300

    Criado campo de busca.

--- Secao11-Django-com-Python-Projetos/Projeto-E-commerce/produto/urls.py
+++ Secao11-Django-com-Python-Projetos/Projeto-E-commerce/produto/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'produto'
@@ -10,4 +11,5 @@ urlpatterns = [
     path('removerdocarrinho/', views.RemoverDoCarrinho.as_view(), name='removerdocarrinho'),
     path('carrinho/', views.Carrinho.as_view(), name='carrinho'),
     path('resumodacompra/', views.ResumoDaCompra.as_view(), name='resumodacompra'),
+    path('busca/', views.Busca.as_view(), name='busca'),
 ]
